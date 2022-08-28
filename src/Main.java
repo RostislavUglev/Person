ed4d6b38268d176b6cd9407e8ecc0e44f1ce0d45
Rostislav Uglev <urg@mail.ru>
2022-08-28 14:47:30 +1000

    add main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Person> listOfPersons = new ArrayList<>();
+        listOfPersons.add(new Person("John", "Smith", 50));
+        listOfPersons.add(new Person("Louie", "de Bourbone", 40));
+        listOfPersons.add(new Person("Pedro", "San Miguel", 30));
+        Collections.sort(listOfPersons, new PersonsComparator());
+        System.out.println(listOfPersons);
+
+    }
 }
